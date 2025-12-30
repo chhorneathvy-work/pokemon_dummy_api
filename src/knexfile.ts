a9d14310fde441386db581f2b6efc75fdee7838e
chhorneath <chhorneathvy.work@gmail.com>
2025-12-30 14:20:47 +0700

    chore: set Node 20 runtime for Railway

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -7,21 +7,21 @@ const isProd = process.env.NODE_ENV === "production";
 const config: { [key: string]: Knex.Config } = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-     migrations: {
+    connection: process.env.DATABASE_URL ?? process.env.DATABASE_PUBLIC_URL,
+    migrations: {
       directory: path.resolve(__dirname, "../migrations"),
-      extension: "ts"
-    }
+      extension: "ts",
+    },
   },
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL ?? process.env.DATABASE_PUBLIC_URL,
     migrations: {
       directory: path.resolve(__dirname, "migrations"),
-      extension: "js"
+      extension: "js",
     },
-    pool: { min: 2, max: 10}
+    pool: { min: 2, max: 10 },
   },
-}
+};
 export default config;
